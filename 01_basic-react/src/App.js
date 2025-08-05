5bf7f4de6e645a92dcde05c9e57e893f302bd6b7
Aalok Kumar Rajwade <aalokkr.rajwade.cer21@itbhu.ac.in>
2025-08-05 17:49:51 +0530

    disecting react

---
+++ 01_basic-react/src/App.js
@@ -0,0 +1,12 @@
+import Temp from "./Temp";
+
+function App() {
+  return (
+    <>
+      <h1>Classic React app</h1>
+      <Temp />
+    </>
+  );
+}
+
+export default App;
---
+++ 01_basic-react/src/Temp.jsx
@@ -0,0 +1,5 @@
+function Temp() {
+  return <h1>Classic Temporary app</h1>;
+}
+
+export default Temp;
